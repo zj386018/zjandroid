84cb68a6dcc319446c7f5ca33dffa4052461a26f
zj386018 <547758478@qq.com>
2016-04-06 20:09:00 +0800

    旋转菜单2

--- workspacesForEnglish/旋转图标/src/com/baiyi/main/MainActivity.java
+++ workspacesForEnglish/旋转图标/src/com/baiyi/main/MainActivity.java
@@ -11,9 +11,10 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	private ImageView iv_home;
-	private RelativeLayout level1,level2,level3;
-	private boolean isShowLeve2 = true;//是否显示二级菜单
+	private ImageView iv_home,iv_menu;
+	private RelativeLayout level1, level2, level3;
+	private boolean isShowLeve2 = true;// 是否显示二级菜单
+	private boolean isShowLeve3 = true;// 是否显示三级菜单
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +26,41 @@ public class MainActivity extends Activity implements OnClickListener {
 	private void initView() {
 		setContentView(R.layout.activity_main);
 		iv_home = (ImageView) findViewById(R.id.iv_home);
-		 level1 = (RelativeLayout) findViewById(R.id.level1);
-		 level2 = (RelativeLayout) findViewById(R.id.level2);
-		 level3 = (RelativeLayout) findViewById(R.id.level3);
+		iv_menu = (ImageView) findViewById(R.id.iv_menu);
+		level1 = (RelativeLayout) findViewById(R.id.level1);
+		level2 = (RelativeLayout) findViewById(R.id.level2);
+		level3 = (RelativeLayout) findViewById(R.id.level3);
 
 	}
 
 	private void initListener() {
 		iv_home.setOnClickListener(this);
+		iv_menu.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View view) {
 		switch (view.getId()) {
 		case R.id.iv_home:
-			if(isShowLeve2){
-				//将执行隐藏第二级菜单的操作
-				
+			if (isShowLeve2) {
+				// 将执行隐藏第二级菜单的操作
 				AnimUtil.closeMenu(level2);
-				
-			}else{
-				//将执行显示第二级菜单的操作
+			} else {
+				// 将执行显示第二级菜单的操作
+				AnimUtil.showMenu(level2);
 			}
 			isShowLeve2 = !isShowLeve2;
 			break;
-
+		case R.id.iv_menu:
+			if (isShowLeve3) {
+				// 将执行隐藏第三级菜单的操作
+				AnimUtil.closeMenu(level3);
+			} else {
+				// 将执行显示第三级菜单的操作
+				AnimUtil.showMenu(level3);
+			}
+			isShowLeve3 = !isShowLeve3;
+			break;
 		default:
 			break;
 		}
--- workspacesForEnglish/旋转图标/src/com/baiyi/util/AnimUtil.java
+++ workspacesForEnglish/旋转图标/src/com/baiyi/util/AnimUtil.java
@@ -17,9 +17,20 @@ public class AnimUtil {
 				RotateAnimation.RELATIVE_TO_SELF, 1);//点在纵轴Y上基于自身长度位置的百分比
 		//设置时间
 		animation.setDuration(500);
+		animation.setFillAfter(true);//让动画结束后，控件保持结束后的状态
 		rl.startAnimation(animation);
+	}
+	
+	public static void showMenu(RelativeLayout rl){
 		
-		
+		//设置基于自身的旋转点
+				RotateAnimation animation = new RotateAnimation(-180, 0, 
+						RotateAnimation.RELATIVE_TO_SELF, 0.5f,//点在横轴X上基于自身长度位置的百分比
+						RotateAnimation.RELATIVE_TO_SELF, 1);//点在纵轴Y上基于自身长度位置的百分比
+				//设置时间
+				animation.setDuration(500);
+				animation.setFillAfter(true);//让动画结束后，控件保持结束后的状态
+				rl.startAnimation(animation);
 	}
 
 }
