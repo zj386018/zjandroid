013a9333471d0199cff9f108c7a818b63c514b56
zj386018 <547758478@qq.com>
2016-04-21 23:59:15 +0800

    自定义下拉菜单选择控件

--- workspacesForEnglish/下拉多选控件/gen/com/baiyi/select/R.java
+++ workspacesForEnglish/下拉多选控件/gen/com/baiyi/select/R.java
@@ -21,13 +21,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int fc1e390d794f6f36064a209ea3b48b51=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int select=0x7f020002;
+        public static final int select1=0x7f020003;
+        public static final int select2=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int editText=0x7f080000;
+        public static final int iv_delete=0x7f080003;
+        public static final int iv_select=0x7f080001;
+        public static final int tv_number=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int adapter_lists=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- workspacesForEnglish/下拉多选控件/src/com/baiyi/select/MainActivity.java
+++ workspacesForEnglish/下拉多选控件/src/com/baiyi/select/MainActivity.java
@@ -1,17 +1,170 @@
 package com.baiyi.select;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private EditText editText;
+	private ImageView iv_select;
+
+	private ArrayList<String> lists = new ArrayList<String>();
+	private MyAdapter adapter;
+	private ListView listView;
+
+	private int popuWindowHeight = 300;
+	private PopupWindow popupWindow;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		initView();
+		initListener();
+		initDate();
 	}
 
-	
+	/**
+	 * 初始化控件
+	 */
+	private void initView() {
+		setContentView(R.layout.activity_main);
+		editText = (EditText) findViewById(R.id.editText);
+		iv_select = (ImageView) findViewById(R.id.iv_select);
+	}
+
+	/**
+	 * 初始化监听事件
+	 */
+	private void initListener() {
+		iv_select.setOnClickListener(this);
+	}
+
+	/**
+	 * 初始化数据
+	 */
+	private void initDate() {
+		for (int i = 10; i < 25; i++) {
+			lists.add("2013" + i + "160" + i);
+		}
+		initListView();
+	}
+
+	/**
+	 * 初始化下拉选项数据
+	 */
+	private void initListView() {
+		listView = new ListView(MainActivity.this);
+		listView.setBackgroundResource(R.drawable.select2);
+		// listView.setVerticalScrollBarEnabled(false);//设置listView的竖直滑动条不可见
+
+		adapter = new MyAdapter();
+
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				editText.setText(lists.get(arg2));
+				popupWindow.dismiss();
+			}
+		});
+
+	}
+
+	private void showNumberList() {
+		if (popupWindow == null) {
+			popupWindow = new PopupWindow(listView, editText.getWidth(),
+					popuWindowHeight);
+		}
+		//让popupWindow里面获得点击焦点，必须同时设置下面三个属性。
+			//设置内部view获取焦点，必须设置为true
+		popupWindow.setFocusable(true);
+			//必须设置背景图片
+		popupWindow.setBackgroundDrawable(new BitmapDrawable());
+			//必须设置点击外部时，popupWindow消失为true
+		popupWindow.setOutsideTouchable(true);
+		
+		popupWindow.showAsDropDown(editText, 0, 0);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+
+		switch (arg0.getId()) {
+		case R.id.iv_select:
+			showNumberList();
+			Toast.makeText(MainActivity.this, "点击下拉", Toast.LENGTH_LONG);
+			break;
+		}
+	}
+
+	class MyAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return lists.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			return lists.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public View getView(final int arg0, View arg1, ViewGroup arg2) {
+			final View view = View.inflate(MainActivity.this,
+					R.layout.adapter_lists, null);
+			TextView textView = (TextView) view.findViewById(R.id.tv_number);
+			ImageView  imageView = (ImageView ) view.findViewById(R.id.iv_delete);
+
+			textView.setText(lists.get(arg0));
+			imageView.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// Log.e("MainActivity", "position="+arg0);
+					lists.remove(arg0);
+					notifyDataSetChanged();// 刷新listView的数据
+					//刷新popuWindow的界面（判断listview的高度是否大于popuWindow的高度）
+					//若大于它，则popuWindow的高度不变。
+					//若小于它，则popuWindow的高度变为listview的高度。
+					int listviewHeight = view.getHeight() * lists.size();
+					popupWindow.update(
+							editText.getWidth(),
+							listviewHeight > popuWindowHeight ? popuWindowHeight
+									: listviewHeight);
+
+					// 若没有数据的时候，popuWindow应该改变为不可见。
+					if (lists.size() == 0) {
+						popupWindow.dismiss();
+						iv_select.setVisibility(View.GONE);
+					}
+				}
+			});
+
+			return view;
+		}
+	}
 
 }
